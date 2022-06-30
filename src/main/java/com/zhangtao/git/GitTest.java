6dba940822f1892f0efae01f35f7e5691699a6f6
ztspark <ztspark@163.com>
2022-06-30 19:43:36 +0800

    second commit

--- src/main/java/com/zhangtao/git/GitTest.java
+++ src/main/java/com/zhangtao/git/GitTest.java
@@ -7,5 +7,6 @@ package com.zhangtao.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println("hello git");
     }
 }
