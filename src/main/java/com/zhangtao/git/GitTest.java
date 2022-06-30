7b51d1e14738a03e06994fa3d845e01022c0ad63
ztspark <ztspark@163.com>
2022-06-30 19:54:12 +0800

    hot-fix commit

--- src/main/java/com/zhangtao/git/GitTest.java
+++ src/main/java/com/zhangtao/git/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
         System.out.println("hello world");
         System.out.println("hello git");
         System.out.println("hello ztspark");
+        System.out.println("github");
     }
 }
